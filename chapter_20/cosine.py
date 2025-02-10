a678936f0b73e82ce86991fc35d2254c1158cafc
Ron Kneusel <ron@kneusel.org>
2025-02-10 09:06:14 -0700

    updated

--- chapter_20/cosine.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def cosine(a,b):
-    """Calculate the cosine distance between two vectors"""
-    num = np.dot(a,b)
-    den = np.sqrt(np.dot(a,a)) * np.sqrt(np.dot(b,b))
-    return 1.0 - num/den
-
